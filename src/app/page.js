dd2e2c7abd3d30c57ceb5bd1e5e790a125b64295
Habeeb-design <Heasybreezy@gmail.com>
2024-10-28 20:15:39 -0700

    feat: added the blue button

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import Button from "./components/Button/Button";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Button color = "yellow" size = "small" text = "beeb" /> 
+      <Button color = "yellow" size = "small" text = "close" /> 
+      <Button color = "blue" size = "log in" text = "enter" /> 
+
+
 
 
 
