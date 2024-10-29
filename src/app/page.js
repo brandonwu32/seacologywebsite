ccaf708660235b648cd199554a24c5953f883566
sophia <scarazoortiz@berkeley.edu>
2024-10-28 22:25:05 -0700

    editied navbar import

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/navbar";
 //hi camila
 export default function Home() {
   return (
