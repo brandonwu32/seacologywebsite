852670a3a599938d92dae8efbd6e9711443b5848
KatiaJacoby <katiajacoby@visitor-10-56-157-52.wifi.berkeley.edu>
2024-10-29 23:11:02 -0700

    made the welcome page

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -3,38 +3,30 @@ import Bubble from "../components/bubble/bubble"
 import Image from "next/image";
 import styles from "./page.css";
 import Navbar from "../components/navbar/navbar";
+import button from "../components/button/button";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <Navbar/>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-        <div className = "bubblie1">
-            <Bubble heading = {"Guidelines"} subtext = {"hi"}/>
-        </div>
-        <div className = "bubblie2">
-            <Bubble heading = {"Project Management"} subtext = {"hi"}/>
+    <div className="page">
+      
+        <Navbar />
+  
 
+      <div className="main-content">
+        <div className="top-bar">
+          <h1 className="welcome-heading">Welcome</h1>
+          <button className="login-button">Log In</button>/
         </div>
-        
-        <div className = "bubblie3">
-            <Bubble heading = {"Financial"} subtext = {"hi"}/>
+        <div className = "bottom-bar"></div>
+        <div className="bubbles-container">
+          <Bubble heading="Guidelines" subtext="blurb" />
+          <Bubble heading="Project Management" subtext="blurb" />
+          <Bubble heading="Financial" subtext="blurb" />
+          <Bubble heading="Contact" subtext="blurb" />
         </div>
-            <Bubble heading = {"Contact"} subtext = {"hi"}/>
-            
       </div>
+    </div>
   );
 }
+
