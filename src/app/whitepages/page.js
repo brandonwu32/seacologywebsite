9d0bc546334cd64c6388974aa06a8f407e85f747
Camila <camilaquezadas@berkeley.edu>
2024-10-30 22:31:13 -0700

    attempting page

--- src/app/whitepages/page.js
+++ src/app/whitepages/page.js
@@ -2,17 +2,27 @@ import Image from "next/image";
 import styles from "./page.css";
 import Button from "../components/button/button";
 import Navbar from "../components/navbar/navbar";
+import whitePageBubble from "../components/whitepagebubble/whitepagebubble";
+
+
 
 export default function Whitepages() {
   return (
-    <div className={styles.page}>
-      <div>
+    <div className="{styles.page}">
+      <div className = "bar">
+        <Navbar />
       </div>
-      <div className = 'team'>
+
+      <div className = 'title'>
         <h1> Team</h1>
       </div>
+
+      <whitePageBubble 
+        src="../components/media/ceopic.jpg"
+        alt="CeoPic"
+        // onClick={() => console.log("Bubble clicked!")}  // Optional: custom onClick function
+      />
       
-      <Button color = "red" size = "small" text = "bello" />
       </div>
   );
 }
