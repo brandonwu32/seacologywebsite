05c38bb5f302b973586fd05105c4bf41e33906eb
Camila <camilaquezadas@berkeley.edu>
2024-11-12 14:50:38 -0800

    added looping for table

--- src/app/components/table/table.js
+++ src/app/components/table/table.js
@@ -1,17 +1,23 @@
-import "./table.css"
+import "./table.css";
 
-export default function Table(props) {
+export default function Table({ projects }) {
     return (
         <table className="custom-table">
             <thead>
                 <tr>
-                    <th className = "tile">Projects</th>
-                    <th className = "tile">Status</th>
-                    <th className = "tile">Latest Update</th>
+                    <th className="tile">Projects</th>
+                    <th className="tile">Status</th>
+                    <th className="tile">Latest Update</th>
                 </tr>
             </thead>  
             <tbody>
-                REPLACE WITH MAP
+                {projects.map((project, index) => (
+                    <tr key={index}>
+                        <td>{project.project}</td>
+                        <td>{project.status}</td>
+                        <td>{project.latestupdate}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
--- src/app/components/whitepagepopup/infopopup/infopopup.js
+++ src/app/components/whitepagepopup/infopopup/infopopup.js
@@ -1,9 +1,8 @@
-import './infopopup.css'
+import './infopopup.css';
 import Image from "next/image";
 import Table from '../../table/table';
-import Button from '../../button/button'
-import WhitePageBubble from '../../whitepagebubbles/popupbubble'
-
+import Button from '../../button/button';
+import WhitePageBubble from '../../whitepagebubbles/popupbubble';
 
 export default function WhitePagePopUp(props) {
     return props.trigger ? (
@@ -12,17 +11,18 @@ export default function WhitePagePopUp(props) {
                 <div className='rectangle'>
                     <p className='name'>{props.name}</p>
                     <hr className="yellowline" />
-                    <div class="bubs">
-                    <WhitePageBubble 
-                    src="https://www.seacology.org/wp-content/uploads/2020/01/duane_snorkel_tonga-scaled-e1579722582118-478x549.jpg"
-                    alt="CeoPic"
-                    className = 'picturr'>
-                    </WhitePageBubble>
+                    <div className="bubs">
+                        <WhitePageBubble 
+                            src="https://www.seacology.org/wp-content/uploads/2020/01/duane_snorkel_tonga-scaled-e1579722582118-478x549.jpg"
+                            alt="CeoPic"
+                            className="picturr"
+                        />
                     </div>
-                    <Table project="Project A" status="In Progress" update="2024-11-03" />
+                    {/* Pass the projects array to the Table component */}
+                    <Table projects={props.projects} />
                 </div>
-                <div className = 'buttonz'>
-                <Button className="blue" id="small" onClick={props.close}>Close</Button>
+                <div className="buttonz">
+                    <Button className="blue" id="small" onClick={props.close}>Close</Button>
                 </div>
             </div>
         </div>
