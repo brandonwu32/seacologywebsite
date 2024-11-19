f75ba68b75bfde6eaf00e9d0eecb671f6da419b4
Brandon Wu <brandonwu32@berkeley.edu>
2024-11-18 23:02:09 -0800

    fixed: welcome page

--- src/Project-Page.js
+++ src/Project-Page.js
@@ -1,19 +1,26 @@
-"use client"
 
 import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from "./components/navbar/navbar";
-import Heading from "./components/info-hub/heading"
-import Welcome from "../app/components/info-hub/pages/welcome/welcome"
-import Step4 from "./components/info-hub/pages/project-management/step4"
+import Button from '../components/button/button';
+import logo from '/assets/seacology_logo.png';
 
-export default function project() {
+export default function Home() {
   return (
-   
-    <div>
-       <Step5/>
+    <div className="page">
+      <div className="image-container">
+      <Image src={logo} alt="Seacology Logo" />
+      </div>
+      <div className="main-content">
+        <div className="top-bar">
+          <div className = "login-area"></div>
+          <Button color = "blue" size = "large" text = "Log in"/>
+        </div>
+        <div className = "bottom-bar"> </div>
+      <div className="button-bar">
+        <Button color="blue" size = "large" text = "Submit Update"/>
+        <Button color="blue" size = "large" text = "Info Hub"/>
+        <Button color="blue" size="large" text="White Pages" />
+      </div>
 
+      </div>
     </div>
-     
-  );
-}
+  )}
--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -1,12 +1,61 @@
+'use client'
 import Bubble from "../components/bubble/bubble"
 
+import styles from "./page.css"
 import Image from "next/image";
-import styles from "./page.css";
 import Navbar from "../components/navbar/navbar";
-import Button from "../components/button/button";
+import Button from '../components/button/button';
+import Heading from "../components/info-hub/heading"
+import Link from "next/link"
 import logo from '/assets/seacology_logo.png';
 
+export default function Welcome() {
+    return (
+        <div>
+            <Navbar/>
+            <div className="page-wrapper">
+                <Heading text="Welcome" buttonText = 'edit'/>
 
+                <div className="welcome-wrapper">
+                    <div className="link-bubble-wrapper">
+                        <Link href="/guidelines">
+                            <Bubble className="welcome-bubble" heading="Guidelines" width={'20rem'} height={'20rem'}/>
+                        </Link>
+                    </div>
+                    <div className="link-bubble-wrapper">
+                        <Link href="/projectmanagement/pm-overview">
+                            <Bubble className="welcome-bubble" heading="Project Management" width={'20rem'} height={'20rem'}/>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="welcome-wrapper">
+                    <div className="link-bubble-wrapper">
+                        <Link href="/financial">
+                            <Bubble className="welcome-bubble" heading="Financial" width={'20rem'} height={'20rem'}/>
+                        </Link>
+                    </div>
+                    <div className="link-bubble-wrapper">
+                        <Link href="/contact">
+                            <Bubble className="welcome-bubble" heading="Contact" width={'20rem'} height={'20rem'}/>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="button-wrapper">
+                    <Link href="">
+                        <Button color="blue" size="small" text="back"/>
+                    </Link>
+
+                    <Link href="/guidelines">
+                        <Button color="blue" size="small" text="next"/>
+                    </Link>
+                </div>
+            </div>
+          </div>
+    )
+  }
+{/*
 export default function Home() {
   return (
     <div className="page">
@@ -18,14 +67,14 @@ export default function Home() {
           <div className = "login-area"></div>
           <Button color = "blue" size = "large" text = "Log in"/>
         </div>
+    );
+}
         <div className = "bottom-bar"> </div>
       <div className="button-bar">
         <Button color="blue" size = "large" text = "Submit Update"/>
         <Button color="blue" size = "large" text = "Info Hub"/>
         <Button color="blue" size="large" text="White Pages" />
       </div>
-        
+
       </div>
-    </div>
-  );
-}
+    </div> */}
