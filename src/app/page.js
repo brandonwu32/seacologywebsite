138f26efc02f548242cbdee2e64dd0f70125bc15
Nishant Malpani <nmalpani@outlook.com>
2024-11-18 22:17:45 -0800

    page.js cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -5,27 +6,9 @@ import Navbar from "./components/navbar/navbar";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <Navbar/>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-        <div className = "bubs">
-      </div>
-      </div>
-      
-
+    <>
+    </>
   );
 }
+
 
